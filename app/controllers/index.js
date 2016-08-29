5deb8ad68f24eb2dad0638334ff49849f30aa739
Dela Cruz James <kris.khaira@gmail.com>
2016-08-29 18:45:47 +0800

    create inValid and isDisabled function

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,5 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isDisabled: true
+  isDisabled: true,
+
+  emailAddress: "",
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid')
+  // actualEmailAddress: Ember.computed('emailAddress', function() { 
+  //   console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+  // }),
+  //
+  // emailAddressChanged: Ember.observer('emailAddress', function() { 
+  //   console.log('observer is called', this.get('emailAddress')); 
+  // })
+
 });
