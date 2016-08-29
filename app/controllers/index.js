407c12cb1e327a195239c50f7e5caf883d5d1c09
Dela Cruz James <kris.khaira@gmail.com>
2016-08-29 19:01:03 +0800

    Add saveInvitation actions on index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,7 +7,15 @@ export default Ember.Controller.extend({
   emailAddress: "",
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: Ember.computed.not('isValid')
+  isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+    saveInvitation() {
+      // alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
   // actualEmailAddress: Ember.computed('emailAddress', function() { 
   //   console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
   // }),
