2b3edbc02a59d61135bd876de17b1e1e52638253
James Dela Cruz <delacruzjamesmartin@gmail.com>
2016-09-02 14:52:05 +0800

    refactor index controller.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,12 +12,14 @@ export default Ember.Controller.extend({
   actions: {
     saveInvitation() {
       const email = this.get('emailAddress');
-      const newInvitation = this.store.createRecord('invitation', {email: email} );
-      newInvitation.save();
+      const newInvitation = this.store.createRecord('invitation', {
+        email: email
+      });
 
-      // alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
+        this.set('emailAddress', '');
+      });
     }
   }
   // actualEmailAddress: Ember.computed('emailAddress', function() { 
