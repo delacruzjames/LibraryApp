d1ec7b38c0c3bb5922feca1fde189e38ee298515
James Dela Cruz <delacruzjamesmartin@gmail.com>
2016-09-02 13:32:48 +0800

    Modify index controller create invitation database

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,15 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isDisabled: true,
-
-  emailAddress: "",
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
+  emailAddress: '',
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   isDisabled: Ember.computed.not('isValid'),
 
   actions: {
     saveInvitation() {
+      const email = this.get('emailAddress');
+      const newInvitation = this.store.createRecord('invitation', {email: email} );
+      newInvitation.save();
+
       // alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
       this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
       this.set('emailAddress', '');
