efb48d5feb2126828e0a7721ce9da12e6753b372
James Dela Cruz <delacruzjamesmartin@gmail.com>
2016-09-03 22:34:40 +0800

    Modify library model

--- app/models/library.js
+++ app/models/library.js
@@ -1,7 +1,11 @@
-import DS from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr'
+import Ember from 'ember';
 
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  address: DS.attr('string'),
-  phone: DS.attr('string')
+export default Model.extend({
+  name: attr('string'),
+  address: attr('string'),
+  phone: attr('string'),
+
+  isValid: Ember.computed.notEmpty('name')
 });
