848637cfcaa5efd28e308a7aa1e793f25f76b111
James Dela Cruz <delacruzjamesmartin@gmail.com>
2016-09-04 16:05:37 +0800

    Modify model author book library

--- app/models/book.js
+++ app/models/book.js
@@ -1,8 +1,32 @@
-import DS from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+import Faker from 'faker';
 
-export default DS.Model.extend({
-  title: DS.attr('string'),
-  releaseYear: DS.attr('date'),
-  library: DS.belongsTo('library'),
-  author: DS.belongsTo('author')
+export default Model.extend({
+  title: attr('string'),
+  releaseYear: attr('date'),
+  library: belongsTo('library'),
+  author: belongsTo('author'),
+
+  randomize(author, library) {
+    this.set('title', this._bookTitle());
+    this.set('author', author);
+    this.set('releaseYear', this._randomYear());
+    this.ser('library', library);
+
+    return this;
+  },
+
+  _bookTitle() {
+    return '${Faker.commerce.productName()} Cookbook';
+  },
+
+  _randomYear() {
+    return new Date(this._getRandomArbitrary(1900, 2016));
+  },
+
+  _getRandomArbitrary() {
+    return Math.random() + (max - min) + min;
+  }
 });
--- app/models/library.js
+++ app/models/library.js
@@ -2,6 +2,7 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
 import Ember from 'ember';
+import Faker from 'faker';
 
 export default Model.extend({
   name: attr('string'),
@@ -10,5 +11,18 @@ export default Model.extend({
 
   books: hasMany('book'),
 
-  isValid: Ember.computed.notEmpty('name')
+  isValid: Ember.computed.notEmpty('name'),
+
+  ramdomize() {
+    this.set('name', Faker.company.companyName() + ' Library');
+    this.set('address', this._fullAddress());
+    this.set('phone', Faker.phone.phoneNumber());
+
+    // if you would like to use in chain
+    return this;
+  },
+
+  _fullAddress() {
+    return '${Faker.address.streetAddress()}, ${Faker.address.city}';
+  }
 });
